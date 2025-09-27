c6b10f382d808397fa368f2865b0408cef321a01
koti-312 <koteswar31204@gmail.com>
2025-09-27 13:49:02 +0530

    Update AddProduct.jsx

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -40,7 +40,7 @@ const AddProduct = () => {
 
         if(responseData.success)
             {
-            product.image=`https://e-commerce-app-backend-31uv.onrender.com${responseData.image_url}`;
+            product.image=responseData.image_url;
             console.log(product);
             await fetch('https://e-commerce-app-backend-31uv.onrender.com/addproduct',{
                 method:'POST',
