f8496ef23aa9ffb7926b5fc7f3ea57c6c9d74c40
koteswar singh <koteswar31204@gmail.com>
2026-09-19 23:32:10 +0530

    feat: show the breadcrums in product

--- frontend/src/Component/Breadcrums/Breadcrum.jsx
+++ frontend/src/Component/Breadcrums/Breadcrum.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import './Breadcrum.css';
+import React from 'react'
+import './Breadcrum.css'
 
 const Breadcrum = ({ product }) => {
 
   if (product) {
-    return <div className="breadcrum"><b>HOME | SHOP</b></div>;
+    return <div className="breadcrum"><b>HOME | SHOP</b></div>
   }
 
   return (
@@ -13,7 +13,7 @@ const Breadcrum = ({ product }) => {
         HOME | SHOP | {product.category} | {product.name}
       </b>
     </div>
-  );
+  )
 }
 
-export default Breadcrum;
+export default Breadcrum
