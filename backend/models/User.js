85dd52c7b7d10697f8d3d0de45b89782f371be84
koteswar singh <koteswar31204@gmail.com>
2026-09-19 21:44:47 +0530

    feat: Add User schema with Mongoose

---
+++ backend/models/User.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String
+  },
+
+  email: {
+    type: String,
+    unique: true
+  },
+
+  password: {
+    type: String
+  },
+
+  cartData: {
+    type: Object
+  },
+  
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+const Users = mongoose.model("Users", userSchema)
+
+export default Users
