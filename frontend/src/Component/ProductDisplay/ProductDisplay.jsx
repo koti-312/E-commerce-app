76705acd1d03ccccf759a26c5d9707b9d1fb8b92
koteswar singh <koteswar31204@gmail.com>
2026-09-19 23:24:54 +0530

    feat: Display all products with details

--- frontend/src/Component/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Component/ProductDisplay/ProductDisplay.jsx
@@ -1,11 +1,11 @@
 import React, { useContext } from 'react'
-import './ProductDisplay.css';
-import { ShopContext } from '../Context/ShopContext';
+import './ProductDisplay.css'
+import { ShopContext } from '../Context/ShopContext'
 
 const ProductDisplay = (props) => {
 
-    const { product } = props;
-    const {addtoCart} = useContext(ShopContext);
+    const { product } = props
+    const { addtoCart } = useContext(ShopContext)
 
     return (
         <div className='productdisplay'>
@@ -15,31 +15,28 @@ const ProductDisplay = (props) => {
                     <img src={product.image} alt="" />
                     <img src={product.image} alt="" />
                     <img src={product.image} alt="" />
-
                 </div>
+
                 <div className="productdisplay-img">
-                    <img src={product.image} alt="" className="productdisplay-main-img" /></div>
-                    <div className="product-details">
+                    <img src={product.image} alt="" className="productdisplay-main-img" />
+                </div>
 
-                <h1 className="name">{product.name}</h1>
-                <h1 className="quality">{product.quality}</h1>
-                <h3 className='head'>p(144)</h3>
-                <h3 className="productdisplay-right-prices">${product.price}</h3>
-                <br/>
-                <h3 className='productdisplay-right-description'>A lightweight cotton dress with soft pastel floral prints. Designed for comfort during warm days, it features a flowing silhouette and breathable fabric. Perfect for beach outings, picnics, or casual evening wear.</h3>
-                <br/>
-                <button onClick={()=>{addtoCart(product.id)}} className='cart'> Add to cart</button>
-                <br/>
-                <p className='head'>category: {product.category}</p>
-                <p className='head'>handpicked Style | Assured quality</p>
+                <div className="product-details">
 
+                    <div className='product-display'>
+                        <h1 className="prod-name">{product.name} </h1>
+                        <span>{product.quality}</span>
+                        <span>p(144)</span>
+                        <span className="productdisplay-right-prices">${product.price}</span>
+                        <p className='productdisplay-right-description'>A lightweight cotton dress with soft pastel floral prints. Designed for comfort during warm days, it features a flowing silhouette and breathable fabric. Perfect for beach outings, picnics, or casual evening wear.</p>
+                        <button onClick={() => { addtoCart(product.id) }} className='cart'> Add to cart</button>
+                        
+                    </div>
                 </div>
             </div>
         </div>
 
-
-
     )
 }
 
-export default ProductDisplay;
+export default ProductDisplay
