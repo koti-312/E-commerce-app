6bcb4f8f085e475c89feff199031d1a02accd207
koteswar singh <koteswar31204@gmail.com>
2026-09-19 21:54:35 +0530

    feat: add authentication middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken"
+
+const fetchUser = async (req, res, next) => {
+    const token = req.header("auth-token")
+
+    if (!token) {
+        res.status(401).send({
+            errors: "Please authenticate using valid token"
+        })
+    }
+    else {
+        try {
+            const data = jwt.verify(token, "secret_ecom")
+            req.user = data.user
+            next()
+        }
+        catch (error) {
+            res.status(401).send({
+                errors: "Please authenticate using valid token"
+            })
+        }
+    }
+}
+
+export default fetchUser
