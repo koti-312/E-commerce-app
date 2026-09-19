b8aee4880bd10591183653862241a31fc05ff57e
koteswar singh <koteswar31204@gmail.com>
2026-09-19 23:13:35 +0530

    feat: Add product name and prices

--- frontend/src/Component/Item/Item.jsx
+++ frontend/src/Component/Item/Item.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import './Item.css';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import './Item.css'
+import { Link } from 'react-router-dom'
 
 const Item = (props) => {
   return (
@@ -16,4 +16,4 @@ const Item = (props) => {
   )
 }
 
-export default Item;
+export default Item
