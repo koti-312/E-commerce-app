8770193d53db51b04363bbb32d104947c392a39c
koteswar singh <koteswar31204@gmail.com>
2026-09-21 22:44:44 +0530

    feat: store the cartdata and removecart

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,79 +2,88 @@ import jwt from "jsonwebtoken"
 import Users from "../models/User.js"
 
 export const signup = async (req, res) => {
+    try {
+        const checkUser = await Users.findOne({
+            email: req.body.email
+        })
 
-    let check = await Users.findOne({
-        email: req.body.email
-    })
+        if (checkUser) {
+            return res.status(400).json({
+                success: false,
+                errors: "Existing user found with same email address"
+            })
+        }
 
-    if (check) {
-        return res.status(400).json({
-            success: false,
-            errors: "existing user found with same email address"
+        const cart = {}
+        for (let i = 0; i < 300; i++) {
+            cart[i] = 0
+        }
+
+        const user = new Users({
+            name: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            cartData: cart
+        })
+
+        const savedUser = await user.save()
+        const data = {
+            user: {
+                id: savedUser.id
+            }
+        }
+
+        const token = jwt.sign(data, "secret_ecom")
+        res.json({
+            success: true,
+            token
         })
     }
-
-    let cart = {}
-
-    for (let i = 0; i < 300; i++) {
-        cart[i] = 0
+    catch (error) {
+        res.status(500).json({
+            success: false,
+            errors: "Something went wrong"
+        })
     }
-
-    const user = new Users({
-        name: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        cartData: cart
-    })
-
-    await user.save()
-
-    const data = {
-        user: {
-            id: user.id
-        }
-    }
-
-    const token = jwt.sign(data, "secret_ecom")
-    res.json({
-        success: true,
-        token
-    })
 }
 
 export const login = async (req, res) => {
-    let user = await Users.findOne({
-        email: req.body.email
-    })
+    try {
+        const user = await Users.findOne({
+            email: req.body.email
+        })
 
-    if (user) {
-
-        const passCompare = req.body.password === user.password
-        if (passCompare) {
-            const data = {
-                user: {
-                    id: user.id
-                }
-            }
-
-            const token = jwt.sign(data, "secret_ecom")
-            res.json({
-                success: true,
-                token
+        if (!user) {
+            return res.json({
+                success: false,
+                errors: "Wrong Email Id"
             })
         }
-        else {
-            res.json({
+
+        const passCompare = req.body.password === user.password
+        if (!passCompare) {
+            return res.json({
                 success: false,
                 errors: "Wrong Password"
             })
         }
-    }
-    else {
+
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+
+        const token = jwt.sign(data, "secret_ecom")
         res.json({
+            success: true,
+            token
+        })
+    }
+    catch (error) {
+        res.status(500).json({
             success: false,
-            errors: "Wrong Email Id"
+            errors: "Something went wrong"
         })
     }
 }
-
