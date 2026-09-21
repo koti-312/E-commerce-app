4353102590700aec7364b41a19524384b18606ef
koteswar singh <koteswar31204@gmail.com>
2026-09-21 22:50:27 +0530

    feat: verify the user token

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,24 +1,23 @@
 import jwt from "jsonwebtoken"
 
 const fetchUser = async (req, res, next) => {
-    const token = req.header("auth-token")
 
+    const token = req.header("auth-token")
     if (!token) {
-        res.status(401).send({
+        return res.status(401).json({
             errors: "Please authenticate using valid token"
         })
     }
-    else {
-        try {
-            const data = jwt.verify(token, "secret_ecom")
-            req.user = data.user
-            next()
-        }
-        catch (error) {
-            res.status(401).send({
-                errors: "Please authenticate using valid token"
-            })
-        }
+
+    try {
+        const data = jwt.verify(token, "secret_ecom")
+        req.user = data.user
+        next()
+    }
+    catch (error) {
+        return res.status(401).json({
+            errors: "Please authenticate using valid token"
+        })
     }
 }
 
