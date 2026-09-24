05578eec1c4e7d8d747e042491b6ec5fd55cf56f
koteswar singh <koteswar31204@gmail.com>
2026-09-24 23:21:39 +0530

    feat: Add bcrypt hash password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,89 +1,105 @@
 import jwt from "jsonwebtoken"
 import Users from "../models/User.js"
+import bcrypt from "bcrypt"
+
 
 export const signup = async (req, res) => {
-    try {
-        const checkUser = await Users.findOne({
-            email: req.body.email
-        })
 
+    try {
+        const { username, email, password } = req.body
+
+        const checkUser = await Users.findOne({ email })
         if (checkUser) {
             return res.status(400).json({
                 success: false,
-                errors: "Existing user found with same email address"
+                message: "Existing user found with same email address"
             })
         }
+
+        const hashedpassword = await bcrypt.hash(password, 10)
 
         const cart = {}
         for (let i = 0; i < 300; i++) {
             cart[i] = 0
         }
 
-        const user = new Users({
-            name: req.body.username,
-            email: req.body.email,
-            password: req.body.password,
+        const userdata = new Users({
+            name: username,
+            email: email,
+            password: hashedpassword,
             cartData: cart
         })
+        const saveUser = await userdata.save()
 
-        const savedUser = await user.save()
         const data = {
             user: {
-                id: savedUser.id
+                id: saveUser.id
             }
         }
 
-        const token = jwt.sign(data, "secret_ecom")
-        res.json({
+        const token = jwt.sign(
+            data,
+            process.env.JWT_SECRET,
+            { expiresIn: "7d" }
+        )
+
+        res.status(201).json({
             success: true,
             token
         })
     }
     catch (error) {
+        console.log("Error:", error.message)
         res.status(500).json({
             success: false,
-            errors: "Something went wrong"
+            message: "Something went wrong"
         })
     }
 }
 
 export const login = async (req, res) => {
-    try {
-        const user = await Users.findOne({
-            email: req.body.email
-        })
 
-        if (!user) {
-            return res.json({
+    try {
+        const { email, password } = req.body
+
+        const checkUser = await Users.findOne({ email })
+
+        if (!-checkUser) {
+            res.status(400).json({
                 success: false,
-                errors: "Wrong Email Id"
+                message: "Wrond email Id"
             })
         }
 
-        const passCompare = req.body.password === user.password
-        if (!passCompare) {
-            return res.json({
+        const comparePassword = await bcrypt.compare(password, checkUser.password)
+        if (!comparePassword) {
+            res.status(400).json({
                 success: false,
-                errors: "Wrong Password"
+                message: "Invaild password"
             })
         }
 
         const data = {
             user: {
-                id: user.id
+                id: checkUser.id
             }
         }
 
-        const token = jwt.sign(data, "secret_ecom")
-        res.json({
+        const token = jwt.sign(
+            data, process.env.JWT_SECRET,
+            { expiresIn: "7d" }
+        )
+
+        res.status(200).json({
             success: true,
             token
         })
     }
     catch (error) {
+        console.log("Error:", error.message)
         res.status(500).json({
             success: false,
-            errors: "Something went wrong"
+            message: "Something went wrong"
         })
     }
 }
